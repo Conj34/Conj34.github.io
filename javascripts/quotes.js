1d041b8c9f694b17c6bda4766831d1d6e1bd5410
Domenico Mergoni <97534935+Conj34@users.noreply.github.com>
2023-07-24 16:13:40 +0100

    Some new things

--- javascripts/quotes.js
+++ javascripts/quotes.js
@@ -1,4 +1,4 @@
-var quotes = ["When I die, I want to go peacefully like my grandfather did–in his sleep. Not yelling and screaming like the passengers in his car.  <p><center>ABC</center></p>", "I have six locks on my door all in a row. When I go out, I lock every other one. I figure no matter how long somebody stands there picking the locks, they are always locking three.  <p><center>DEF</center></p>"]
+var quotes = ["When I die, I want to go peacefully like my grandfather did–in his sleep. Not yelling and screaming like the passengers in his car.  <p><center>ABC</center></p>", "I have six locks on my door all in a row. When I go out, I lock every other one. I figure no matter how long somebody stands there picking the locks, they are always locking three.  <p><center>DEF</center></p>", "Be realistic, demand the impossible.  <p><center><i>-Che Guevara-</i></center></p>"]
 var quote;
 function newQuote() {
   var randomNumber = Math.floor(Math.random() * (quotes.length))
