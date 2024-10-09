55b630c3689f2783a762aa378eb6b6de5d85b519
Domenico Mergoni <97534935+Conj34@users.noreply.github.com>
2024-10-09 20:07:33 +0100

    Update without documents
    Update without documents

--- _site/_site/javascripts/passwd.js
+++
@@ -1,9 +0,0 @@
-function passwd_1(){
-
-  var password = prompt('Enter the password to download the file:');
-  if(password.toLowerCase() == "ciao"){
-    window.open("_site/images/logo.png")    
-  }else{
-    alert("incorrect password!! please try again");
-  }
-}
